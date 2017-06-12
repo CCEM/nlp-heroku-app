7e4e8657f6f709c481c69f4da13c0214887be671
Chris Hudson <c.ahudson84@yahoo.com>
2017-06-12 09:40:59 -0700

    starting up basic pyramid framework.

---
+++ reddex/reddex/routes.py
@@ -0,0 +1,7 @@
+"""Assign web url routes to the names of different pages."""
+
+
+def includeme(config):
+    """Assign web url routes to the names of different pages."""
+    config.add_static_view('static', 'reddex:static')
+    config.add_route('home', '/')
