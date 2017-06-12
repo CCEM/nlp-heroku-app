6cce8bfb9d8ee788eacfd2037d1748f87f5c68a0
Chris Hudson <c.ahudson84@yahoo.com>
2017-06-12 10:29:25 -0700

    fixed missplaced files.

--- reddex/reddex/routes.py
+++ reddex/reddex/routes.py
@@ -5,3 +5,4 @@ def includeme(config):
     """Assign web url routes to the names of different pages."""
     config.add_static_view('static', 'reddex:static')
     config.add_route('home', '/')
+    config.add_route('inbound', '/inbound')
