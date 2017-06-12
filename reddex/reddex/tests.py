57950c9c4f351fbcd213511f5413391e2ea557df
Morgan Nomura <morganelle@gmail.com>
2017-06-12 15:35:31 -0700

    initial work on analyzing sentiment

--- reddex/reddex/tests.py
+++ reddex/reddex/tests.py
@@ -3,6 +3,10 @@ from pyramid import testing
 from reddex.views.default import inbound_view
 import pytest
 
+SAMPLE_POSTS = [
+    {'reddex0': "I hate cake.", 'reddex1': "Dogs are cute."}
+]
+
 
 @pytest.fixture
 def dummy_request():
@@ -14,4 +18,9 @@ def test_inbound_returns_dict(dummy_request):
     """."""
     dummy_request.method = 'POST'
     response = inbound_view(dummy_request)
-    assert response == {}
+    assert type(response) is dict
+
+
+def test_inbound_handles_data(dummy_request):
+    """."""
+    pass
